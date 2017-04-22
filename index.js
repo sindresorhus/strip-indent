dbeb23243094331b847d3c296a4915ff0b05f3b6
Sindre Sorhus <sindresorhus@gmail.com>
2017-04-23 02:29:36 +0700

    Meta tweaks

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ module.exports = str => {
 		return str;
 	}
 
-	// TODO: use spread operator when targeting Node.js 6
-	const indent = Math.min.apply(Math, match.map(x => x.length)); // eslint-disable-line
+	// TODO: Use spread operator when targeting Node.js 6
+	const indent = Math.min.apply(Math, match.map(x => x.length));
 	const re = new RegExp(`^[ \\t]{${indent}}`, 'gm');
 
 	return indent > 0 ? str.replace(re, '') : str;
