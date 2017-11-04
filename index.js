089df03598849be53568d01dc92e530adf02f051
James Kyle <me@thejameskyle.com>
2017-11-04 16:59:45 +1100

    Use min-indent (#4)

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
-module.exports = str => {
-	const match = str.match(/^[ \t]*(?=\S)/gm);
+const minIndent = require('min-indent');
 
-	if (!match) {
+module.exports = str => {
+	const indent = minIndent(str);
+
+	if (indent === 0) {
 		return str;
 	}
 
-	// TODO: Use spread operator when targeting Node.js 6
-	const indent = Math.min.apply(Math, match.map(x => x.length));
 	const re = new RegExp(`^[ \\t]{${indent}}`, 'gm');
 
-	return indent > 0 ? str.replace(re, '') : str;
+	return str.replace(re, '');
 };
